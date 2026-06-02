20a11deb557481c6fc85b036795cb7c0caaa3572
chozza88 <a94557934@gmail.com>
2026-06-02 18:41:02 +0900

    260601 [develop] 상세페이지 완료

--- src/main/java/com/msa4meerkatgram/global/errors/GlobalExceptionHandler.java
+++ src/main/java/com/msa4meerkatgram/global/errors/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.msa4meerkatgram.global.errors;
 
+import com.msa4meerkatgram.global.errors.custom.DeletedRecordException;
 import com.msa4meerkatgram.global.errors.custom.InvalidTokenException;
 import com.msa4meerkatgram.global.errors.custom.NotRegisteredException;
 import com.msa4meerkatgram.global.responses.GlobalRes;
@@ -56,6 +57,17 @@ public class GlobalExceptionHandler {
                 GlobalRes.<String>builder()
                         .code("E04")
                         .message("토큰 이상")
+                        .data(e.getMessage())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(DeletedRecordException.class)
+    public ResponseEntity<GlobalRes<String>> deletedRecordHandle(DeletedRecordException e){
+        return ResponseEntity.status(404).body(
+                GlobalRes.<String>builder()
+                        .code("E10")
+                        .message("DELETED_RECORD_ERROR")
                         .data(e.getMessage())
                         .build()
         );
